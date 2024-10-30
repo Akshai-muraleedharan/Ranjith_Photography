ec7d0654ae9ff3d5a8c75430b928ad2fb1369efd
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-10-30 06:12:44 +0000

    hero page section mobile view

--- frontend/src/components/sections/HeroSection.jsx
+++ frontend/src/components/sections/HeroSection.jsx
@@ -3,8 +3,16 @@ import images from '../../assets/image/test.jpg'
 function HeroSection() {
   return (
     <div>
-      <div className='hero_image' style={{background:`url(${images})`}}></div>
-     <p>dsdsdasds</p>
+      <div className='hero_image relative' style={{background:`url(${images})`}}>
+        <div className='absolute bottom-20 text-center w-full'>
+            
+        <h2 className='text-4xl font-semibold text-white'>Capturing Moments</h2>
+     <h3 className='mt-2 text-end mr-4 text-2xl font-semibold text-white'>Creating Memories</h3>
+
+     <h4 className='text-[18px] mt-4 font-semibold text-white'>Ranjith Babu_Photography</h4>
+        </div>
+      </div>
+    
     </div>
   )
 }
