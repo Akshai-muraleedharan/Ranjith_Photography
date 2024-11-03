1707333abc262a4150c020c9e1192aafab4b09c1
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-11-03 13:48:19 +0000

    hero section tablet view

--- frontend/src/components/sections/HeroSection.jsx
+++ frontend/src/components/sections/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import images from '../../assets/image/IMG-20241014-WA0038.jpg'
+import TabletImage from '../../assets/image/IMG-20241103-WA0021.jpg'
 function HeroSection() {
   return (
     <div className='relative'>
@@ -18,17 +19,25 @@ function HeroSection() {
        
       </div>
     
-    <div className='px-10 py-8 tablet_Container flex justify-between lg:justify-evenly'>
+    <div className='tablet_Container relative'>
         {/* text container */}
-        <div className='flex flex-col justify-center'>
+        {/* <div className='flex flex-col justify-center'>
         <h2 className='text-4xl font-bold lg:text-6xl'>Capturing Moments</h2>
      <h3 className='mt-1 text-end mr-4 text-2xl font-bold lg:text-4xl lg:mt-2'>Creating Memories</h3>
 
      <h4 className='text-[16px] mt-4 font-semibold text-end'>Ranjith Babu_Photography</h4>
-        </div>
+        </div> */}
 
        
-        <div className='tablet_view rounded my-3' style={{background:`url(${images})`}}></div>
+        <div className='tablet_view  ' style={{background:`url(${TabletImage})`}}>
+        <div className="  image_overlay"></div>
+      <div className='absolute bottom-[10%] flex justify-center flex-col items-center w-full text-white'>
+            <h2 className='text-4xl font-bold md:text-4xl lg:text-6xl'>Capturing Moments</h2>
+          <h3 className='mt-1  mr-4 text-2xl md:text-2xl font-bold lg:text-4xl lg:mt-2'>Creating Memories</h3>
+
+          <h4 className='text-[16px] mt-4 font-semibold '>Ranjith Babu_Photography</h4>
+      </div>
+        </div>
     </div>
     </div>
 
